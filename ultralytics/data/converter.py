fd2a83dc73e8694d62afa71d90e54081d2d83180
Edgardo Solano Carrillo <Edgardo.SolanoCarrillo@dlr.de>
2024-02-21 12:01:34 +0100

    Modify convert_coco to have links to imgs in same dir as labels

--- ultralytics/data/converter.py
+++ ultralytics/data/converter.py
@@ -1,5 +1,6 @@
 # Ultralytics YOLO 🚀, AGPL-3.0 license
 
+import os
 import json
 from collections import defaultdict
 from pathlib import Path
@@ -215,7 +216,7 @@ def coco80_to_coco91_class():
 
 def convert_coco(
     labels_dir="../coco/annotations/",
-    save_dir="coco_converted/",
+    save_dir="yolo_labels/",
     use_segments=False,
     use_keypoints=False,
     cls91to80=True,
@@ -251,8 +252,11 @@ def convert_coco(
 
     # Import json
     for json_file in sorted(Path(labels_dir).resolve().glob("*.json")):
-        fn = Path(save_dir) / "labels" / json_file.stem.replace("instances_", "")  # folder name
+        split = json_file.stem.replace("instances_", "")
+        fn = Path(save_dir) / "labels" / split  # folder name
+        fi = Path(save_dir) / "images" / split  # folder name
         fn.mkdir(parents=True, exist_ok=True)
+        fi.mkdir(parents=True, exist_ok=True)
         with open(json_file) as f:
             data = json.load(f)
 
@@ -303,8 +307,11 @@ def convert_coco(
                             box + (np.array(ann["keypoints"]).reshape(-1, 3) / np.array([w, h, 1])).reshape(-1).tolist()
                         )
 
-            # Write
-            with open((fn / f).with_suffix(".txt"), "a") as file:
+            # Write images
+            os.system(f"ln -s {f} {fi / Path(str(f).replace('/', '_'))}")
+            
+            # Write labels
+            with open((fn / Path(str(f).replace('/', '_'))).with_suffix('.txt'), 'a') as file:
                 for i in range(len(bboxes)):
                     if use_keypoints:
                         line = (*(keypoints[i]),)  # cls, box, keypoints
